ec42d71aa481181dbe4ace82f8d7bfe5edcbbc8d
alec <xenomote@gmail.com>
2019-02-05 11:53:55 +0000

    untested NFA

--- Automata.hs
+++ Automata.hs
@@ -1,18 +1,21 @@
 module Automata where
 
+    import Control.Monad
+
     import Automata.Deterministic
     import qualified Automata.Nondeterministic as NFA
 
-    convert :: Traversable t => NFA.Automata t s a -> Automata (t s) a
+    convert :: (Monad t, Foldable t) => NFA.Automata t s a -> Automata (t s) a
     convert m = Automata initial transition accepts where
 
-        initial :: t s  
-        initial = undefined
+        initial = return $ NFA.initial m
 
-        accepts = any (NFA.accepts m)
+        accepts = any $ NFA.accepts m
 
-        transition :: a -> t s -> t s
-        transition a s = undefined
+        transition a s = join $ fmap (m `NFA.transition` a) s
+
+        -- accepts :: t s -> Bool
+        -- transition :: m -> a -> s -> t s
 
     elements :: (Enum a, Bounded a) => [a]
     elements = [minBound ..]
--- Automata/Nondeterministic.hs
+++ Automata/Nondeterministic.hs
@@ -1,7 +1,12 @@
 module Automata.Nondeterministic where
 
+    import Data.Foldable
+
     data Automata f s a = Automata {
         initial    :: s,
-        transition :: s -> a -> f s,
+        transition :: a -> s -> f s,
         accepts    :: s -> Bool
     }
+
+    scan :: (Monad f, Foldable f, Foldable g) => Automata f s a -> g a -> f s
+    scan m w = foldl' (flip $ transition m) (initial m) w
